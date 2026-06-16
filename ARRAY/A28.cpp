4a00ec724fae3ce46b0787bf211a796f450e3be3
pavanprajapati123 <pavanprajapati7586@gmail.com>
2026-06-16 12:37:21 +0530

    array

---
+++ ARRAY/A28.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+
+        long long first = LLONG_MIN;
+        long long second = LLONG_MIN;
+        long long third = LLONG_MIN;
+
+        for (int num : nums) {
+
+            // Skip duplicate values
+            if (num == first || num == second || num == third)
+                continue;
+
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            }
+            else if (num > second) {
+                third = second;
+                second = num;
+            }
+            else if (num > third) {
+                third = num;
+            }
+        }
+
+        return (third == LLONG_MIN) ? first : third;
+    }
+};
+
+int main() {
+
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    Solution obj;
+
+    cout << "Third Maximum Number = "
+         << obj.thirdMax(nums)
+         << endl;
+
+    return 0;
+}
