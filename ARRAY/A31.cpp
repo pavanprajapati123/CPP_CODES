7430341f822b8bf29a8042f25b9bab4a51d3dbee
pavanprajapati123 <pavanprajapati7586@gmail.com>
2026-06-17 18:53:21 +0530

    array

---
+++ ARRAY/A31.cpp
@@ -0,0 +1,53 @@
+// 724. Find Pivot Index
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+
+        int totalSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    Solution obj;
+
+    cout << "Pivot Index = "
+         << obj.pivotIndex(nums)
+         << endl;
+
+    return 0;
+}
