49511b4cb207519df8a9e4a18f5c6faaf1a806a5
pavanprajapati123 <pavanprajapati7586@gmail.com>
2026-06-17 23:45:52 +0530

    string

---
+++ STRING/S11.cpp
@@ -0,0 +1,25 @@
+//8. Character Frequency
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    char ch;
+    int count = 0;
+
+    cout << "Enter string: ";
+    getline(cin, str);
+
+    cout << "Enter character: ";
+    cin >> ch;
+
+    for(char c : str) {
+        if(c == ch)
+            count++;
+    }
+
+    cout << "Frequency = " << count;
+
+    return 0;
+}
