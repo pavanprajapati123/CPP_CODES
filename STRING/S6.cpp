12b16ae146ef1e7252a7bf7a31d75c08f954de64
pavanprajapati123 <pavanprajapati7586@gmail.com>
2026-06-17 23:34:28 +0530

    string

---
+++ STRING/S6.cpp
@@ -0,0 +1,15 @@
+//3. Do Strings Ko Join Karna
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string first = "Hello";
+    string second = " World";
+
+    string result = first + second;
+
+    cout << result;
+
+    return 0;
+}
