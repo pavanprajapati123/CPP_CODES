6c15d0725e8e239d182f72c56653f765c034055e
pavanprajapati123 <pavanprajapati7586@gmail.com>
2026-06-17 21:06:22 +0530

    string+ two pointer

---
+++ STRING/S3.cpp
@@ -0,0 +1,53 @@
+// 125. Valid Palindrome
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        int st = 0;
+        int end = s.length() - 1;
+
+        while (st < end) {
+
+            if (!isalnum(s[st])) {
+                st++;
+                continue;
+            }
+
+            if (!isalnum(s[end])) {
+                end--;
+                continue;
+            }
+
+            if (tolower(s[st]) != tolower(s[end])) {
+                return false;
+            }
+
+            st++;
+            end--;
+        }
+
+        return true;
+    }
+};
+
+int main() {
+
+    string s;
+
+    cout << "Enter String: ";
+    getline(cin, s);
+
+    Solution obj;
+
+    if (obj.isPalindrome(s))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+
+    return 0;
+}
