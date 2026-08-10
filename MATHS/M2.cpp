79c2917a9beaa79948893d3c4a5c43ff049eadef
pavanprajapati123 <pavanprajapati7586@gmail.com>
2026-08-10 19:26:01 +0530

    Winner Square Game Using Dynamic Programming

---
+++ MATHS/M2.cpp
@@ -0,0 +1,45 @@
+//Stone Game / Winner Square Game
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool winnerSquareGame(int n)
+    {
+        vector<bool> dp(n + 1, false);
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j * j <= i; j++)
+            {
+                // If after taking j*j stones,
+                // opponent is in losing state
+                if(!dp[i - j * j])
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
+
+int main()
+{
+    int n;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    Solution obj;
+
+    if(obj.winnerSquareGame(n))
+        cout << "Alice Wins";
+    else
+        cout << "Bob Wins";
+
+    return 0;
+}
