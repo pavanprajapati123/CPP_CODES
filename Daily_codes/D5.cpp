82846257175e5830b03446fe30541ebbfd55b0ff
pavanprajapati123 <pavanprajapati7586@gmail.com>
2026-08-21 21:46:30 +0530

    K-th Smallest Number Using Binary Search and Inclusion-Exclusion

---
+++ Daily_codes/D5.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+
+    long long gcd(long long a, long long b)
+    {
+        while(b)
+        {
+            long long temp = a % b;
+            a = b;
+            b = temp;
+        }
+
+        return a;
+    }
+
+    long long lcm(long long a, long long b)
+    {
+        return a / gcd(a, b) * b;
+    }
+
+    long long count(long long x, vector<int>& coins)
+    {
+        int n = coins.size();
+        long long total = 0;
+
+        // Inclusion-Exclusion
+        for(int mask = 1; mask < (1 << n); mask++)
+        {
+            long long L = 1;
+
+            for(int i = 0; i < n; i++)
+            {
+                if(mask & (1 << i))
+                {
+                    L = lcm(L, coins[i]);
+
+                    if(L > x)
+                        break;
+                }
+            }
+
+            if(L > x)
+                continue;
+
+            int bits = __builtin_popcount(mask);
+
+            if(bits % 2 == 1)
+                total += x / L;
+            else
+                total -= x / L;
+        }
+
+        return total;
+    }
+
+    long long findKthSmallest(vector<int>& coins, int k)
+    {
+        long long low = 1;
+
+        long long high =
+            1LL * (*min_element(coins.begin(), coins.end())) * k;
+
+        while(low < high)
+        {
+            long long mid =
+                low + (high - low) / 2;
+
+            if(count(mid, coins) >= k)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+};
+
+int main()
+{
+    int n, k;
+
+    cout << "Enter number of coins: ";
+    cin >> n;
+
+    vector<int> coins(n);
+
+    cout << "Enter coin values: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> coins[i];
+    }
+
+    cout << "Enter k: ";
+    cin >> k;
+
+    Solution obj;
+
+    cout << "K-th Smallest Number = "
+         << obj.findKthSmallest(coins, k);
+
+    return 0;
+}
