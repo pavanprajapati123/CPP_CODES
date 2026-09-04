ef9ac343f41e29ef11915d0f00ffbe3b11866ec3
pavanprajapati123 <pavanprajapati7586@gmail.com>
2026-09-04 20:17:25 +0530

    First Stable Index using Prefix Max and Suffix Min

---
+++ ARRAY/A99.cpp
@@ -0,0 +1,64 @@
+//3903. Smallest Stable Index I
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int firstStableIndex(vector<int>& nums, int k)
+    {
+        int n = nums.size();
+
+        vector<int> suffixMin(n);
+
+        suffixMin[n - 1] = nums[n - 1];
+
+        for(int i = n - 2; i >= 0; i--)
+        {
+            suffixMin[i] = min(nums[i], suffixMin[i + 1]);
+        }
+
+        int prefixMax = nums[0];
+
+        for(int i = 0; i < n; i++)
+        {
+            prefixMax = max(prefixMax, nums[i]);
+
+            int score = prefixMax - suffixMin[i];
+
+            if(score <= k)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main()
+{
+    int n, k;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter array elements: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    cout << "Enter k: ";
+    cin >> k;
+
+    Solution obj;
+
+    cout << "First Stable Index: "
+         << obj.firstStableIndex(nums, k);
+
+    return 0;
+}
