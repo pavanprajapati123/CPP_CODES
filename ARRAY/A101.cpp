4c14d3216008535b9e5841042c217651d135d32b
pavanprajapati123 <pavanprajapati7586@gmail.com>
2026-09-10 19:40:15 +0530

    Wave Form Array using Sorting

--- ARRAY/A101.cpp
+++ ARRAY/A101.cpp
@@ -1,98 +1,37 @@
-//Search Word in Matrix
+//
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution
 {
 public:
-    vector<vector<int>> searchWord(vector<vector<char>>& mat, string& word)
+    vector<int> waveFormArray(vector<int> &arr, int n)
     {
-        int n = mat.size();
-        int m = mat[0].size();
+        sort(arr.begin(), arr.end());
 
-        vector<vector<int>> ans;
-
-        // 8 possible directions
-        int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n - 1; i += 2)
         {
-            for(int j = 0; j < m; j++)
-            {
-                // First character doesn't match
-                if(mat[i][j] != word[0])
-                    continue;
-
-                bool found = false;
-
-                // Check all 8 directions
-                for(int d = 0; d < 8 && !found; d++)
-                {
-                    int x = i;
-                    int y = j;
-                    int k;
-
-                    for(k = 1; k < word.size(); k++)
-                    {
-                        x += dx[d];
-                        y += dy[d];
-
-                        // Out of matrix
-                        if(x < 0 || x >= n || y < 0 || y >= m)
-                            break;
-
-                        // Character doesn't match
-                        if(mat[x][y] != word[k])
-                            break;
-                    }
-
-                    if(k == word.size())
-                        found = true;
-                }
-
-                if(found)
-                    ans.push_back({i, j});
-            }
+            swap(arr[i], arr[i + 1]);
         }
 
-        return ans;
+        return arr;
     }
 };
 
 int main()
 {
-    int n, m;
-
-    cout << "Enter rows and columns: ";
-    cin >> n >> m;
-
-    vector<vector<char>> mat(n, vector<char>(m));
-
-    cout << "Enter matrix characters: ";
-
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < m; j++)
-        {
-            cin >> mat[i][j];
-        }
-    }
-
-    string word;
-
-    cout << "Enter word: ";
-    cin >> word;
-
     Solution obj;
 
-    vector<vector<int>> ans = obj.searchWord(mat, word);
+    vector<int> arr = {1, 2, 3, 4, 5, 6};
+    int n = arr.size();
 
-    cout << "Starting Positions: ";
+    vector<int> result = obj.waveFormArray(arr, n);
 
-    for(auto p : ans)
+    cout << "Wave Form Array: ";
+
+    for (int x : result)
     {
-        cout << "(" << p[0] << ", " << p[1] << ") ";
+        cout << x << " ";
     }
 
     return 0;
